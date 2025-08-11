467c48bbf373268a2a8bb50f25822e4342d1391b
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-11 16:16:48 -0500

    complete twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,27 @@
+
+// LeetCode: Two Sum
+// This solution uses a HashMap to store visited numbers and their indices.
+// For each number, it calculates the complement (target - current number)
+// and checks if the complement is already in the map. If found, returns the
+// pair of indices; otherwise, stores the current number in the map.
+// Time Complexity: O(n), Space Complexity: O(n).
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer,Integer> map=new HashMap<>();      
+       
+        for(int i=0;i<nums.length;i++){ 
+            int complement=target-nums[i];
+            if(!map.containsKey(complement)){
+                  map.put(nums[i],i);
+                  
+            }
+            else{
+                return new int [] {map.get(complement),i};
+            }
+          
+        }
+        return new int []{0,0};
+        
+    }
+}
