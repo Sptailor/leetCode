0ffe757f9c8451097f097c0e50f210ca88c71385
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-12 16:42:32 -0500

    Solved

---
+++ rmvDuplicate.java
@@ -0,0 +1,16 @@
+// Removes duplicates from a sorted array in place using the two-pointer technique.
+// Overwrites duplicates and returns the new length of the unique portion.
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       
+       int writeIndex=1;
+       for(int i=1;i<nums.length;i++){
+        if(nums[i]!=nums[i-1]){
+            nums[writeIndex]=nums[i];
+            writeIndex++;
+        }
+       }
+       return writeIndex;
+    }
+}
