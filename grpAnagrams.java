19a4987499f2125c32734e97cdfae9dd21b294ff
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-14 17:19:27 -0500

    completed

---
+++ grpAnagrams.java
@@ -0,0 +1,25 @@
+// Groups a list of strings into lists of anagrams using a HashMap.
+// Sorts each string alphabetically to use as a key for grouping.
+// Stores original strings in lists mapped to their sorted form.
+// Returns all grouped anagram lists as a list of lists.
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+
+
+        for( String str:strs){
+           char []strArray=str.toCharArray();
+            Arrays.sort(strArray);
+            String sorted=new String(strArray);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<String>());
+            }
+            map.get(sorted).add(str);
+           
+        }
+
+        return new ArrayList<>(map.values());      
+}
+}
