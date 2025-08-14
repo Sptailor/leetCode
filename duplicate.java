3bcda74cce18f0dabf2409f8c3378565667ccb7e
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-14 18:13:00 -0500

    completed

---
+++ duplicate.java
@@ -0,0 +1,20 @@
+// Checks if an array contains any duplicate elements using a HashSet.
+// Returns true upon finding a duplicate, otherwise false.
+// Runs in O(n) time and O(n) space.
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+
+        }
+
+        return false;
+        
+    }
+}
