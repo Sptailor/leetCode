8f8a72fc7a459c6806aa84f020e4e759daa93637
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-15 15:10:17 -0500

    complete

---
+++ mostWater.java
@@ -0,0 +1,31 @@
+
+// Finds the maximum water container area in an array of heights.
+// Uses the two-pointer approach, starting at both ends of the array.
+// Moves the pointer with the smaller height inward each step.
+// Returns the largest area calculated between any two lines.
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+
+        while(left<right){
+            int leftBar=height[left];
+            int rightBar=height[right];
+            int currArea=0;
+
+           if(leftBar>rightBar){
+            currArea=rightBar *(right-left);
+            right--;
+
+           }else{
+             currArea=leftBar *(right-left);
+             left++;
+           }
+           maxArea=Math.max(maxArea,currArea);
+        }
+        return maxArea;
+
+    }
+}
