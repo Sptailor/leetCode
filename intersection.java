1b8192f51df2e474a817a68471a13a2a29b54233
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-15 13:10:43 -0500

    version 1

---
+++ intersection.java
@@ -0,0 +1,44 @@
+
+//Finds common integer keys between two sets and returns them as an array
+//creates two maps to only look at unique values
+//loops over smaller map
+//converts arrayList to array
+
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> map1 = new HashSet<>();
+        HashSet<Integer> map2 = new HashSet<>();
+
+        for (int num : nums1) {
+            map1.add(num);
+        }
+        for (int num : nums2) {
+            map2.add(num);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if (map1.size() > map2.size()) {
+
+            for (int key : map2) {
+                if (map1.contains(key)) {
+                    list.add(key);
+
+                }
+            }
+        } else {
+            for (int key : map1) {
+                if (map2.contains(key)) {
+                    list.add(key);
+
+                }
+            }
+
+        }
+
+        int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+        return result;
+    }
+}
