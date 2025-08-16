82253c8e13b342e7cfdb7d4ee7f7579d729166cd
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-16 17:59:02 -0500

    Complete

---
+++ hasCycle_twoPtr.java
@@ -0,0 +1,33 @@
+
+
+// Detects if a linked list has a cycle using two pointers (Floyd’s algorithm).
+// Slow pointer moves one step at a time, fast pointer moves two steps.
+// If the list has a cycle, the fast pointer will eventually meet the slow pointer.
+// Returns false if the fast pointer reaches the end of the list (no cycle).
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
