1b4392b4244407b4b4b2206d98371a6a241cf465
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-16 18:17:53 -0500

    Complete

---
+++ treeDepth_recur.java
@@ -0,0 +1,31 @@
+// Recursively computes the maximum depth of a binary tree.
+// Base case: return 0 if the node is null.
+// Otherwise, find the max depth of the left and right subtrees
+// and return 1 + the larger of the two.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        if (root==null)return 0;
+        int leftDepth=maxDepth(root.left);
+        int rightDepth=maxDepth(root.right);
+        return 1+ Math.max(leftDepth,rightDepth);
+
+       
+    }
+}
