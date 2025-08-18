56dbc7232b9df7a44b92871cfa0c1e757bc0a267
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-17 19:44:10 -0500

    Complete

---
+++ validParentheses.java
@@ -0,0 +1,44 @@
+
+
+// This solution uses a stack to validate balanced parentheses.
+// Every time an opening bracket is found, it is pushed onto the stack.
+// For each closing bracket, we check if it matches the top of the stack.
+// If it matches, we pop it; otherwise the string is invalid.
+// At the end, the stack must be empty for the string to be valid.
+
+
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack <Character>stack=new Stack<>();
+
+        for(char c:s.toCharArray()){
+            if(c=='['||c=='('||c=='{'){
+                stack.push(c);
+
+            }
+            else if(!stack.isEmpty()){
+              if ((c == ')' && stack.peek() == '(') ||
+               (c == ']' && stack.peek() == '[') ||
+               (c == '}' && stack.peek() == '{')) {
+               stack.pop();
+            } else {
+              return false;
+}
+
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+
+}
+}
+
+
+
+
+
+
