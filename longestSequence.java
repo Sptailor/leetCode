2a74b03b7a4af4e6e5779a067f79aec6e40e9930
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-08-18 19:05:14 -0500

    complete

---
+++ longestSequence.java
@@ -0,0 +1,29 @@
+
+/**
+ * Uses a HashSet to find the longest streak of consecutive numbers.
+ * Only starts counting when a number is the beginning of a sequence,
+ * then expands forward to count its length. 
+ * Runs in O(n) time.
+ */
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer>set=new HashSet();
+        int longest=0;
+        for(int num:nums){
+            set.add(num);
+        }
+        for(int num: set){
+            if(!set.contains(num-1)){
+                int curr=num;
+                int streak=1;
+                while(set.contains(curr+1)){
+                    streak++;
+                    curr++;
+                }
+                longest=Math.max(longest,streak);
+            }
+        }
+        return longest;
+    }
+}
