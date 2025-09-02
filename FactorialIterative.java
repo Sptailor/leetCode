b9f66bd7849bcadb0a309c3e010de6b772967a9b
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-09-01 21:29:10 -0500

    comment

--- FactorialIterative.java
+++ FactorialIterative.java
@@ -1,4 +1,4 @@
-
+//By Suhail Tailor
 // Program to calculate the factorial of a number.
 // Demonstrates both iterative (loop) and recursive approaches.
 // Factorial of n (n!) = n × (n-1) × (n-2) ... × 1
