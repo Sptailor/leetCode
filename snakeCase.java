bfca33136bd6d04b62cc5cc6c1451bb0b3762942
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-09-06 23:06:16 -0400

    using String builder

--- snakeCase.java
+++ snakeCase.java
@@ -4,18 +4,32 @@
 
 public class snakeCase {
 
-    public static String toSnakeCase(String input) {
+   public static String toSnakeCase(String input) {
         if (input == null || input.isEmpty()) {
             return input;
         }
 
-        // Handle camelCase (insert underscores before uppercase letters)
-        String snake = input.replaceAll("([a-z])([A-Z])", "$1_$2");
+        StringBuilder result = new StringBuilder();
 
-        // Replace spaces or hyphens with underscores
-        snake = snake.replaceAll("[\\s-]+", "_");
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
 
-        // Convert to lowercase
-        return snake.toLowerCase();
+            // If uppercase, add underscore before (if not first char)
+            if (Character.isUpperCase(c)) {
+                if (i > 0) {
+                    result.append('_');
+                }
+                result.append(Character.toLowerCase(c));
+            }
+            // If space or non-letter/digit, replace with underscore
+            else if (!Character.isLetterOrDigit(c)) {
+                result.append('_');
+            }
+            else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
     }
 }
