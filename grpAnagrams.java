8fedc64567d0b96d0f25a0e7931d815682f29c9e
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-09-26 21:40:24 -0500

    updating comments

--- grpAnagrams.java
+++ grpAnagrams.java
@@ -10,21 +10,21 @@ import java.util.List;
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        HashMap<String,ArrayList<String>>map=new HashMap<>();//map to store sorted string as key and list of anagrams as value
 
 
-        for( String str:strs){
-           char []strArray=str.toCharArray();
-            Arrays.sort(strArray);
-            String sorted=new String(strArray);
+        for( String str:strs){//loop over input strings
+           char []strArray=str.toCharArray();//convert string to char array
+            Arrays.sort(strArray);//sort char array
+            String sorted=new String(strArray);//convert sorted char array back to string
 
-            if(!map.containsKey(sorted)){
-                map.put(sorted,new ArrayList<String>());
+            if(!map.containsKey(sorted)){//check if map contains sorted string
+                map.put(sorted,new ArrayList<String>());//if not, add it with an empty list
             }
-            map.get(sorted).add(str);
-           
+            map.get(sorted).add(str);//add original string to the corresponding list
+
         }
 
-        return new ArrayList<>(map.values());      
+        return new ArrayList<>(map.values());      //return all lists of anagrams
 }
 }
