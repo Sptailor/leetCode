ac2bb9f2b66632f135fb87c0a539e11c7addc1e8
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-09-26 21:38:35 -0500

    updating comments

--- intersection.java
+++ intersection.java
@@ -10,31 +10,31 @@ import java.util.HashSet;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
-        HashSet<Integer> map1 = new HashSet<>();
-        HashSet<Integer> map2 = new HashSet<>();
+        HashSet<Integer> map1 = new HashSet<>();//map for nums1
+        HashSet<Integer> map2 = new HashSet<>();//map for nums2 
 
-        for (int num : nums1) {
+        for (int num : nums1) {//populate maps with unique values
             map1.add(num);
         }
-        for (int num : nums2) {
+        for (int num : nums2) {//populate maps with unique values
             map2.add(num);
         }
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();//list to store intersection values
 
-         if (map1.size() > map2.size()) {
+         if (map1.size() > map2.size()) {//swap to loop over smaller map
             HashSet<Integer> temp = map1;
             map1 = map2;
             map2 = temp;
         }
-        for (int key : map1) {
+        for (int key : map1) {//loop over smaller map
             if (map2.contains(key)) {
                 list.add(key);
             }
         }
 
 
-        int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] result = list.stream().mapToInt(Integer::intValue).toArray();//convert arrayList to arrays
         return result;
     }
 }
