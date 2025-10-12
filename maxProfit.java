8e032939a5a81a8626123124cf15c513d6d6cef2
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-10-11 23:06:36 -0500

    comments

--- maxProfit.java
+++ maxProfit.java
@@ -10,11 +10,11 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice=prices[0];
-        int potentialProfit=0;
+        int minPrice=prices[0];// Initialize minPrice to the first price
+        int potentialProfit=0;// Variable to track potential profit
         int maxProfit=0;
-        for(int i=0;i<prices.length;i++){
-            if(minPrice>prices[i]){
+        for(int i=0;i<prices.length;i++){// Iterate through each price in the array
+            if(minPrice>prices[i]){// Update minPrice if the current price is lower
                 minPrice=prices[i];
             }
 
