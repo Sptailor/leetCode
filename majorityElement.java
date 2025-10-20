bb7179b45690485c7ae66e9b65c07895c5a48d41
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-10-20 14:21:00 -0400

    comments

--- majorityElement.java
+++ majorityElement.java
@@ -12,14 +12,14 @@ class Solution {
         HashMap<Integer,Integer>map=new HashMap<>();// Map to store frequency of each element
 
         for(int num: nums){// Populate the frequency map
-            map.put(num,map.getOrDefault(num,0)+1);
+            map.put(num,map.getOrDefault(num,0)+1);// Increment count for each occurrence
         }
 
-        for (int key : map.keySet()) {
-        if (map.get(key) > targetCount) {
-        return key;
+        for (int key : map.keySet()) {// Check for the majority element
+        if (map.get(key) > targetCount) {// If count exceeds n/2, return the element
+        return key;// Return the majority element
          }
          }
-return -1;
+return -1;// Return -1 if no majority element is found (theoretically unreachable)
     
 }}
