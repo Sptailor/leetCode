c199317b1983c5b48a3f932aacbaf8c69c549dad
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-10-25 20:05:29 -0500

    Comment while reviewing

--- recursive_fact.java
+++ recursive_fact.java
@@ -9,7 +9,7 @@ public class recursive_fact {
     // Method to calculate factorial using recursion
     public static int factorial(int n) {
         if (n == 0 || n == 1) {   // base case
-            return 1;
+            return 1;// factorial of 0 or 1 is 1
         }
         return n * factorial(n - 1);  // recursive call
     }
