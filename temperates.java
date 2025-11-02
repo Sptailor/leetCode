a878b3db973c7991db0d59d46f38b20addd18fa9
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2025-11-01 19:05:13 -0500

    Comment while reviewing

--- temperates.java
+++ temperates.java
@@ -17,8 +17,8 @@ class Solution {
 
        for(int i=0;i<temperatures.length;i++){//iterate through temperatures
         while(!stack.isEmpty()&& temperatures[i] > temperatures[stack.peek()] ){//found a warmer temperature
-             int prevIndex = stack.pop();
-             result[prevIndex] = i - prevIndex;
+             int prevIndex = stack.pop();//index of previous cooler temperature
+             result[prevIndex] = i - prevIndex;//calculate days until warmer temperature
         }
         stack.push(i);
 
