85b546b53329a8ccc9d5768fa5859612a6ce36b6
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2026-02-14 22:16:39 -0600

    Add frequency count loop and buckets array

--- javascript/topKFrequent.js
+++ javascript/topKFrequent.js
@@ -8,4 +8,9 @@ var topKFrequent = function(nums, k) {
     const map={};
     const result=[];
 
+    for(const num of nums){
+        map[num]=(map[num]||0)+1;
+    }
+    const buckets=Array(nums.length+1).fill().map(()=>[]);
+
 };
