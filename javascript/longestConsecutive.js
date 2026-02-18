bde72bad38e65ad24058a9e5968aec5537e1fd89
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2026-02-17 20:32:21 -0600

    Refactor code formatting and move max check outside inner loop

--- javascript/longestConsecutive.js
+++ javascript/longestConsecutive.js
@@ -2,31 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
+var longestConsecutive = function (nums) {
+  const map = new Set();
+  let start = 0;
+  let max = 0;
+  let length = 0;
 
-    const map= new Set();
-    let start=0;
-    let max=0;
-    let length=0;
-
-    for( let num of nums){
-        map.add(num);
-
+  for (let num of nums) {
+    map.add(num);
+  }
+  for (let num of map) {
+    if (!map.has(num - 1)) {
+      start = num;
+      length = 1;
+      for (let i = num; map.has(i + 1); i++) {
+        length++;
+      }
+      if (length > max) {
+        max = length;
+      }
     }
-    for(let num of map){
-        if(!map.has(num-1)){
-            start=num;
-            length=1;
-            for(let i=num;map.has(i+1) ; i++){
-                length++;
-                if(length>max){
-                max=length;
-            }
-            }
-
-
-        }
-
-    }
-    return max;
+  }
+  return max;
 };
