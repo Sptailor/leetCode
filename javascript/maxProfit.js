10f63976f2bdbd96358b0cdb0611b11a9502070b
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2026-02-22 18:25:12 -0600

    Add loop with price difference

--- javascript/maxProfit.js
+++ javascript/maxProfit.js
@@ -6,4 +6,9 @@ var maxProfit = function(prices) {
 
     let profit=0;
     let left=0;
+
+
+    for(let right=1;right<prices.length;right++){
+        let tempDiff=prices[right]-prices[left];
+    }
 };
